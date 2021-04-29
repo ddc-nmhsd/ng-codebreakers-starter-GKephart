bcd2e74cb9c3a0b61ff0596495aaed169642d524
George Kephart <gkephart@cnm.edu>
2021-04-29 09:28:54 -0600

    Wired the individual code to display in the detailed-code view

--- ng/src/app/detailed-code/detailed-code.component.ts
+++ ng/src/app/detailed-code/detailed-code.component.ts
@@ -1,17 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {CodeService} from '../shared/services/code.service';
+import {Code} from '../shared/classes/Code';
 @Component({templateUrl:"./detailed-code.component.html"})
 
-export class DetailedCodeComponent {
+export class DetailedCodeComponent implements OnInit{
 	/**
 	 * The codeId grabbed from the url, the value is set in the constructor and cannot be updated after it has been initialized
 	 * @var  string codeId
 	 */
 	private readonly codeId: string;
 
-	constructor(private currentRoute: ActivatedRoute) {
+	public code: null | Code = null;
+
+	constructor(private currentRoute: ActivatedRoute, private codeService: CodeService) {
 		// this.route.snapshot grabs pertinent url parameters of off the url on object instantiation.
 		// <string> asserts that what we get back from currentRoute.snapshot.paramMap.get('codeId') will be a string.
 		this.codeId = <string>this.currentRoute.snapshot.paramMap.get('codeId');
 	}
+
+	ngOnInit() {
+		this.setCodeByCodeId(this.codeId);
+	}
+
+	setCodeByCodeId(codeId: string) : void {
+		this.codeService.getCodeByCodeId(this.codeId).subscribe(response => {
+			this.code = response;
+		})
+	}
+
+
 }
