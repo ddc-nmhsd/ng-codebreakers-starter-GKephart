a9aa0470bbc50dde97c49a04ca59b44e3afbab22
George Kephart <gkephart@cnm.edu>
2021-04-29 10:23:17 -0600

    Wire all the guesses for a code to display in the detailed-code view.

--- ng/src/app/detailed-code/detailed-code.component.ts
+++ ng/src/app/detailed-code/detailed-code.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {CodeService} from '../shared/services/code.service';
 import {Code} from '../shared/classes/Code';
+import {GuessService} from '../shared/services/guess.service';
+import {Guess} from '../shared/classes/Guess';
 @Component({templateUrl:"./detailed-code.component.html"})
 
 export class DetailedCodeComponent implements OnInit{
@@ -13,7 +15,9 @@ export class DetailedCodeComponent implements OnInit{
 
 	public code: null | Code = null;
 
-	constructor(private currentRoute: ActivatedRoute, private codeService: CodeService) {
+	public guesses : Guess[] = [];
+
+	constructor(private currentRoute: ActivatedRoute, private codeService: CodeService, private guessService: GuessService) {
 		// this.route.snapshot grabs pertinent url parameters of off the url on object instantiation.
 		// <string> asserts that what we get back from currentRoute.snapshot.paramMap.get('codeId') will be a string.
 		this.codeId = <string>this.currentRoute.snapshot.paramMap.get('codeId');
@@ -21,12 +25,19 @@ export class DetailedCodeComponent implements OnInit{
 
 	ngOnInit() {
 		this.setCodeByCodeId(this.codeId);
+		this.setGuessesByCodeId(this.codeId);
 	}
 
 	setCodeByCodeId(codeId: string) : void {
 		this.codeService.getCodeByCodeId(this.codeId).subscribe(response => {
 			this.code = response;
 		})
+	}
+
+	setGuessesByCodeId(codeId: string): void {
+		this.guessService.getGuessByCodeId(codeId).subscribe(response => {
+			this.guesses = response;
+		} )
 	}
 
 
