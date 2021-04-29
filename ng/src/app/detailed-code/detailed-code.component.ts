11f320750c1ce03c73f01642724bbda062242a30
George Kephart <gkephart@cnm.edu>
2021-04-29 13:38:29 -0600

    Got the form working and wired

--- ng/src/app/detailed-code/detailed-code.component.ts
+++ ng/src/app/detailed-code/detailed-code.component.ts
@@ -4,6 +4,7 @@ import {CodeService} from '../shared/services/code.service';
 import {Code} from '../shared/classes/Code';
 import {GuessService} from '../shared/services/guess.service';
 import {Guess} from '../shared/classes/Guess';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({templateUrl:"./detailed-code.component.html"})
 
 export class DetailedCodeComponent implements OnInit{
@@ -17,18 +18,23 @@ export class DetailedCodeComponent implements OnInit{
 
 	public guesses : Guess[] = [];
 
-	constructor(private currentRoute: ActivatedRoute, private codeService: CodeService, private guessService: GuessService) {
+	public guessForm: FormGroup;
+
+	constructor(private currentRoute: ActivatedRoute, private codeService: CodeService, private guessService: GuessService, private formBuilder: FormBuilder) {
 		// this.route.snapshot grabs pertinent url parameters of off the url on object instantiation.
 		// <string> asserts that what we get back from currentRoute.snapshot.paramMap.get('codeId') will be a string.
 		this.codeId = <string>this.currentRoute.snapshot.paramMap.get('codeId');
+		this.guessForm = formBuilder.group({
+			guess: ["", [Validators.required]]
+		})
 	}
 
 	ngOnInit() {
-		this.setCodeByCodeId(this.codeId);
+		this.setCodeByCodeId();
 		this.setGuessesByCodeId(this.codeId);
 	}
 
-	setCodeByCodeId(codeId: string) : void {
+	setCodeByCodeId() : void {
 		this.codeService.getCodeByCodeId(this.codeId).subscribe(response => {
 			this.code = response;
 		})
@@ -38,6 +44,21 @@ export class DetailedCodeComponent implements OnInit{
 		this.guessService.getGuessByCodeId(codeId).subscribe(response => {
 			this.guesses = response;
 		} )
+	}
+
+	submitGuess() {
+		const guess: Guess = {
+			id: null,
+			created: null,
+			text: this.guessForm.value.guess,
+			exactMatches: null,
+			nearMatches: null,
+			solution: false
+		};
+
+		this.guessService.createGuess(this.codeId, guess).subscribe(reply => {
+			alert(reply.solution);
+		});
 	}
 
 
